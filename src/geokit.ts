1e53f891529f5370de42b7033f4a2a9ed6c76067
Michael Solati <mkslt04@gmail.com>
2017-10-01 11:19:51 -0400

    feat: implement distance function for two coordinates

--- src/geokit.ts
+++ src/geokit.ts
@@ -3,6 +3,18 @@ import { LatLngLiteral } from './interfaces';
 export class Geokit {
   readonly _base32chars: string = '0123456789bcdefghjkmnpqrstuvwxyz';
 
+  public distance(start: LatLngLiteral, end: LatLngLiteral, unit?: string): number {
+    const radius: number = (unit === 'miles') ? 3963 : 6371;
+    const dLat: number = this._toRad(end.lat - start.lat);
+    const dLon: number = this._toRad(end.lng - start.lng);
+    const lat1: number = this._toRad(start.lat);
+    const lat2: number = this._toRad(end.lat);
+    const a: number = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return (radius * c);
+  }
+
   public hash(coordinates: LatLngLiteral, precision: number = 10): string {
     let hash: string = '';
     let latRng: number[] = [-90, 90];
@@ -25,5 +37,9 @@ export class Geokit {
   private _getBit(point: number, range: number[]): number {
     const middle: number = (range[0] + range[1]) / 2;
     return (middle > point) ? 0 : 1;
+  }
+
+  private _toRad(degrees: number): number {
+    return (degrees * Math.PI / 180);
   }
 }
